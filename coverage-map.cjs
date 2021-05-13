fbceda020bd8346560e9da0168d40b1602694832
esdmr <esdmr0@gmail.com>
2021-05-13 23:07:38 +0430

    Improve on coverage-map regex

--- coverage-map.cjs
+++ coverage-map.cjs
@@ -1,3 +1,2 @@
-const regexJoined = /^build\/test\/(.*)\.d\/.*\.(?:ts|js)/u;
-const regexFile = /^build\/test\/(.*)\.(?:ts|js)/u;
-module.exports = (testFile) => testFile.replace(regexJoined, 'build/$1.js').replace(regexFile, 'build/$1.js');
+const regex = /^build\/test\/(?<dir>(?:[^/]+(?<!\.d)\/)*)(?:(?<dpath>[^/]+?)\.d.*|(?<fpath>[^/]+)\.js)$/u;
+module.exports = (testFile) => testFile.replace(regex, 'build/$<dir>$<dpath>$<fpath>.js');
