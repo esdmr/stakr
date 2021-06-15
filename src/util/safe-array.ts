80605000e35e4941bf79fcb8dd47b5252234a914
esdmr <esdmr0@gmail.com>
2021-06-15 21:59:29 +0430

    Improve on safe-array

--- src/util/safe-array.ts
+++ src/util/safe-array.ts
@@ -1,28 +1,39 @@
+/* eslint-disable @typescript-eslint/non-nullable-type-assertion-style */
 const enum Message {
-	MAX_IS_NOT_INT = 'Maximum length is not a safe (positive) integer',
-	MAX_IS_NEGATIVE = 'Maximum length is negative',
-	STACK_IS_FULL = 'Stack is full',
-	STACK_IS_EMPTY = 'Stack is empty',
-	INDEX_OUT_OF_BOUNDS = 'Index is out of bounds',
-	VALUE_IS_UNDEFINED = 'Undefined values are not allowed in a SafeArray',
-	INDEX_IS_NOT_INT = 'Index is not a safe (positive) integer',
 	INDEX_IS_NEGATIVE = 'Index is negative',
+	INDEX_IS_NOT_INT = 'Index is not a safe (positive) integer',
+	INDEX_OUT_OF_BOUNDS = 'Index is out of bounds',
+	LARGER_THAN_MAX = 'Input array is larger than maximum length allowed',
+	MAX_IS_NEGATIVE = 'Maximum length is negative',
+	MAX_IS_NOT_INT = 'Maximum length is not a safe (positive) integer',
+	STACK_IS_EMPTY = 'Stack is empty',
+	STACK_IS_FULL = 'Stack is full',
 }
 
 /**
- * Dense array with bound checks.
+ * A dense array with bound checks.
  */
 export default class SafeArray<T> {
-	private readonly array: T[] = [];
+	private readonly _array: T[] = [];
+	private readonly _maxLength: number;
 	private _length = 0;
 
+	/**
+	 * The length of a safe-array. This is a number one higher than the highest
+	 * index in the safe-array.
+	*/
 	get length (): number {
 		return this._length;
 	}
 
-	constructor (
-		private readonly maxLength = Number.POSITIVE_INFINITY,
-	) {
+	/**
+	 * @throws {RangeError}
+	 * @param maxLength Maximum number of elements allowed in the safe-array.
+	 * Must be a positive safe integer. (`+Infinity` for no limitation)
+	 */
+	constructor (maxLength = Number.POSITIVE_INFINITY) {
+		this._maxLength = maxLength;
+
 		if (!Number.isSafeInteger(maxLength) && maxLength !== Number.POSITIVE_INFINITY) {
 			throw new RangeError(Message.MAX_IS_NOT_INT);
 		}
@@ -32,77 +43,120 @@ export default class SafeArray<T> {
 		}
 	}
 
-	toNewArray (): T[] {
-		return [...this.array];
+	/**
+	 * Creates a new instance of safe-array and copies a safe-array into it.
+	 *
+	 * @throws {RangeError}
+	 * @param array The array to copy into the safe-array. It should not have
+	 * more elements than the provided maximum length.
+	 * @param maxLength Maximum number of elements allowed in the safe-array.
+	 * Must be a positive safe integer. (`+Infinity` for no limitation)
+	 * @returns The new instance of safe-array.
+	 */
+	static from<T> (array: readonly T[], maxLength?: number): SafeArray<T> {
+		if (typeof maxLength === 'number' && array.length > maxLength) {
+			throw new RangeError(Message.LARGER_THAN_MAX);
+		}
+
+		const safeArray = new SafeArray<T>(maxLength);
+		safeArray.push(...array);
+
+		return safeArray;
 	}
 
+	/**
+	 * Copies the content of a safe-array into a new array.
+	 *
+	 * @returns The new array.
+	 */
+	toNewArray (): T[] {
+		return [...this._array];
+	}
+
+	/**
+	 * Appends new elements to the end of a safe-array.
+	 *
+	 * @throws {RangeError}
+	 * @param items New elements to add to the safe-array.
+	 */
 	push (...items: readonly T[]): void {
-		if (this._length + items.length > this.maxLength) {
+		if (this._length + items.length > this._maxLength) {
 			throw new RangeError(Message.STACK_IS_FULL);
 		}
 
-		for (const item of items) {
-			this.assertNotUndefined(item);
-		}
-
-		this._length = this.array.push(...items);
+		this._length = this._array.push(...items);
 	}
 
+	/**
+	 * Removes the last element from a safe-array and returns it. If the
+     * safe-array is empty, a RangeError is thrown.
+	 *
+	 * @throws {RangeError}
+	 */
 	pop (): T {
-		const value = this.array.pop();
-
-		if (value === undefined) {
+		if (this._array.length === 0) {
 			throw new RangeError(Message.STACK_IS_EMPTY);
 		}
 
-		return value;
+		return this._array.pop() as T;
 	}
 
+	/**
+	 * Reads an element from a safe-array and returns it.
+	 *
+	 * @throws {RangeError}
+	 * @param index Index of element to return.
+	 * @returns Value of element.
+	 */
 	get (index: number): T {
-		const value = this.array[this.normalizeIndex(index)];
-
-		if (value === undefined) {
-			throw new RangeError(Message.INDEX_OUT_OF_BOUNDS);
-		}
-
-		return value;
+		this._assertIndex(index);
+		return this._array[index] as T;
 	}
 
+	/**
+	 * Sets an element to a safe-array.
+	 *
+	 * @throws {RangeError}
+	 * @param index Index of element to change.
+	 * @param value New value to set to the element.
+	 */
 	set (index: number, value: T): void {
-		const normalized = this.normalizeIndex(index);
-		this.assertNotUndefined(value);
-
-		if (normalized >= this.length) {
-			throw new RangeError(Message.INDEX_OUT_OF_BOUNDS);
-		}
-
-		this.array[normalized] = value;
+		this._assertIndex(index);
+		this._array[index] = value;
 	}
 
+	/**
+	 * Empties a safe-array.
+	 */
 	clear (): void {
-		this.array.length = 0;
+		this._array.length = 0;
 		this._length = 0;
 	}
 
+	/**
+	 * Returns a string representation of a safe-array.
+	 *
+	 * @returns The string.
+	 */
 	toString (): string {
-		return this.array.toString();
+		return this._array.toString();
 	}
 
+	/**
+	 * Returns a string representation of a safe-array. The elements are
+	 * converted to string using their toLocalString methods.
+	 *
+	 * @returns The string.
+	 */
 	toLocaleString (): string {
-		return this.array.toLocaleString();
+		return this._array.toLocaleString();
 	}
 
-	get [Symbol.iterator] () {
-		return this.array[Symbol.iterator];
+	get [Symbol.iterator] (): () => IterableIterator<T> {
+		return this._array[Symbol.iterator];
 	}
 
-	private assertNotUndefined (value: T) {
-		if (value === undefined) {
-			throw new TypeError(Message.VALUE_IS_UNDEFINED);
-		}
-	}
-
-	private normalizeIndex (index: number) {
+	private _assertIndex (index: number): void {
 		if (!Number.isSafeInteger(index)) {
 			throw new RangeError(Message.INDEX_IS_NOT_INT);
 		}
@@ -111,6 +165,8 @@ export default class SafeArray<T> {
 			throw new RangeError(Message.INDEX_IS_NEGATIVE);
 		}
 
-		return index;
+		if (index >= this.length) {
+			throw new RangeError(Message.INDEX_OUT_OF_BOUNDS);
+		}
 	}
 }
